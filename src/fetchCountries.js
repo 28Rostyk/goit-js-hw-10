4f7c046474de5a23aba869ba902dd83cd98f0bcf
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-12-03 12:55:41 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,12 @@
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(status)
-    .then(r => {
-      return r.json();
-    });
-}
-
-function status(res) {
-  if (!res.ok) {
-    throw new Error(res.statusText);
-  }
-  return res;
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
 
 // import axios from 'axios';
